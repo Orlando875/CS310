e3696d7f01398e830813c249a6d4fd662594743d
Orlando875 <olara@my365.bellevue.edu>
2024-09-04 21:33:26 -0500

    Add files via upload

---
+++ Lara-M2 Discussion Input-Output Code.cpp
@@ -0,0 +1,22 @@
+
+// The program takes two integers as inputs from the user and outputs their sum
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int num1, num2;
+    
+    std::cout << "Enter the first number: ";
+    std::cin >> num1;
+    
+    std::cout << "Enter the second number: ";
+    std::cin >> num2;
+    
+    int sum = num1 + num2
+    
+    std::cout << "The sum is: " << sum << std::end;
+
+    return 0;
+}
