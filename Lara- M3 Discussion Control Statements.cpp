48cb59067fcf4d7b0520950b512a3702695d3c11
Orlando875 <olara@my365.bellevue.edu>
2024-09-11 20:49:23 -0500

    Add files via upload

---
+++ Lara- M3 Discussion Control Statements.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num;
+    cout << "Enter a number: ";
+    cin >> num;
+
+    if (num > 10 {
+        cout << "The number is greater than 10." << endl;
+    } 
+    else if (num == 10) {
+        cout << "The number is equal to 10." << endl;
+    }
+    else {
+        cout << "The number is less than 10." << endl;
+    }
+
+    prnt << "This line should print but won't due to an error." << endl;
+
+    return 0;
+}
