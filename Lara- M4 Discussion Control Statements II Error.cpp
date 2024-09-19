c0e00a3a54da1a314a7f2789f295b3180553cfaf
Orlando875 <olara@my365.bellevue.edu>
2024-09-18 22:44:44 -0500

    Add files via upload

---
+++ Lara- M4 Discussion Control Statements II Error.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num;
+    cout << "Enter a number: ";
+    cin >> num;
+
+    while num < 5 {
+        cout << "The number is: " << num << endl;
+        num++;
+    }
+
+    for i < 10 {
+        cout << "i is: " << i << endl;
+    }
+
+    return 0;
+}
